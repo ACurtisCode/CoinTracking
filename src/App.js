bce7e8bf9f41c67025836af4b8040a469cd7638f
Alex Curtis <alex.curtis14@gmail.com>
2023-08-26 17:44:11 -0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Coin from './components/Coin';
 import axios from 'axios';
 import Iframe from 'react-iframe';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Main from './components/Main';
 import { Login } from './components/Login';
 
@@ -12,13 +13,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router basename={window.location.pathname || ''}>
+      <HashRouter>
+      <Router>
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route path="/main" element={<Main />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
+      </HashRouter>
     </div>
   )
 }
