cedfd7ce9f202c5a3075f3a024827268fb4936bf
Alex Curtis <alex.curtis14@gmail.com>
2023-08-26 17:45:58 -0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import Coin from './components/Coin';
 import axios from 'axios';
 import Iframe from 'react-iframe';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Main from './components/Main';
 import { Login } from './components/Login';
